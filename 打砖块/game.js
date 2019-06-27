c94772c58c482f18f4c6794a3777714045fe8267
Clinkz.M <2148839177@qq.com>
2019-06-28 00:47:04 +0800

    直播1 跟着写到 37:05

---
+++ 打砖块/game.js
@@ -0,0 +1,64 @@
+var log = console.log.bind(console)
+
+// 32:49
+var imageFromPath = function(path) {
+    var img = new Image()
+    img.src = path
+    return img
+}
+
+// 31:50
+var Paddle = function() {
+    var image = imageFromPath('paddle.png')
+    var o = {
+        image: image,
+        x: 100,
+        y: 200,
+        speed: 5,
+    }
+    return o
+}
+
+var __main = function() {
+    var canvas = document.querySelector('#id-canvas')
+    var context = canvas.getContext('2d')
+
+    var paddle = Paddle()
+
+    var leftDown = false
+    var rightDown = false
+
+    // events
+    window.addEventListener('keydown', function(event) {
+        var k = event.key
+        if (k == 'a') {
+            leftDown = true
+        } else if (k == 'd') {
+            rightDown = true
+        }
+    })
+
+    window.addEventListener('keyup', function(event) {
+        var k = event.key
+        if (k == 'a') {
+            leftDown = false
+        } else if (k == 'd') {
+            rightDown = false
+        }
+    })
+
+    setInterval(function() {
+        // update
+        if (leftDown) {
+            paddle.x = paddle.x - paddle.speed
+        } else if (rightDown) {
+            paddle.x = paddle.x + paddle.speed
+        }
+        // 24:23, 待办：做一个判断，挡板不能超过 canvas 边界
+
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        context.drawImage(paddle.image, paddle.x, paddle.y)
+    }, 1000/30)
+}
+
+__main()
