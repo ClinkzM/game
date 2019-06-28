e519d4555922a8d2aec53a361692a2d3ae57740f
Clinkz.M <2148839177@qq.com>
2019-06-28 17:22:23 +0800

    把代码放到不同文件

---
+++ 打砖块/ball.js
@@ -0,0 +1,40 @@
+var Ball = function() {
+    var image = imageFromPath('ball.png')
+    var o = {
+        image: image,
+        x: 100,
+        y: 200,
+        // 01:06:28
+        speedX: 10,
+        speedY: 10,
+        fired: false,
+    }
+
+    o.move = function() {
+        if (o.fired) {
+            // log('ball move')
+            // 01:07:16
+            if (o.x < 0 || o.x > 400) {
+                o.speedX = -o.speedX
+            }
+            if (o.y < 0 || o.y > 300) {
+                o.speedY = -o.speedY
+            }
+            // move
+            o.x = o.x + o.speedX
+            o.y = o.y + o.speedY
+
+        }
+    }
+
+    o.fire = function() {
+        o.fired = true
+    }
+    o.rebound = function() {
+        o.speedY = -o.speedY
+        // o.speedX = -o.speedX
+    }
+
+    // 直播 - 1 24:23, 待办：做一个判断，挡板不能超过 canvas 边界
+    return o
+}
---
+++ 打砖块/paddle.js
@@ -0,0 +1,36 @@
+var Paddle = function() {
+    var image = imageFromPath('paddle.png')
+    var o = {
+        image: image,
+        x: 100,
+        y: 250,
+        speed: 15,
+    }
+    o.move = function(x) {
+        if (x < 0) {
+            x = 0
+        }
+        if (x > 400 - o.image.width) {
+            x = 400 - o.image.width
+        }
+        o.x = x
+    }
+    o.moveLeft = function() {
+        var left = o.x - o.speed
+        o.move(left)
+    }
+    o.moveRight = function() {
+        var right = o.x + o.speed
+        o.move(right)
+    }
+    o.collide = function(ball) {
+        if (ball.y + ball.image.height > o.y) {
+            if (ball.x > o.x && ball.x < o.x + o.image.width) {
+                // log('相撞')
+                return true
+            }
+        }
+        return false
+    }
+    return o
+}
