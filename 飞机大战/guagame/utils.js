9d902033c8b294f5aa6e54b7811d5e93f6832a41
ClinkzM <2148839177@qq.com>
2019-07-10 18:42:03 +0800

    utils 加绑定所有 input

--- 飞机大战/guagame/utils.js
+++ 飞机大战/guagame/utils.js
@@ -1,11 +1,25 @@
-var e = sel => document.querySelector(sel)
-
 var log = console.log.bind(console)
 
 var imageFromPath = function(path) {
     var img = new Image()
     img.src = path
     return img
+}
+
+var e = sel => document.querySelector(sel)
+
+var es = sel => document.querySelectorAll(sel)
+
+var bindEvent = function(element, eventName, callback) {
+    element.addEventListener(eventName, callback)
+}
+
+var bindAll = function(selector, eventName, callback) {
+    var elements = es(selector)
+    for (var i = 0; i < elements.length; i++) {
+        var e = elements[i]
+        bindEvent(e, eventName, callback)
+    }
 }
 
 var rectIntersects = function(a, b) {
