c6ec18cbd09f8efcd11264e04dfdf9bf94a751b9
Clinkz.M <2148839177@qq.com>
2019-07-11 21:25:25 +0800

    直播 9 - 22:54

--- 飞机大战/scene/title/scene_title.js
+++ 飞机大战/scene/title/scene_title.js
@@ -1,3 +1,40 @@
+class GuaParticle extends GuaImage {
+    constructor() {
+        super(game)
+    }
+}
+
+class GuaParticleSystem {
+    constructor(game) {
+        this.game = game
+        this.setup()
+    }
+    static new(game) {
+        return new this(game)
+    }
+    setup() {
+        this.x = 150
+        this.y = 200
+        this.numberOfParticles = 20
+        this.particles = []
+    }
+    update() {
+        // 添加小火花
+
+
+        // 更新所有的小火花
+        for(var p of this.particles) {
+            p.update()
+        }
+    }
+    draw() {
+        for(var p of this.particles) {
+            p.draw()
+        }
+    }
+}
+
+
 class GuaLabel {
     constructor(game, text) {
         this.game = game
@@ -7,7 +44,8 @@ class GuaLabel {
         return new this(game, text)
     }
     draw() {
-        // this.game.context.fiil
+        this.game.context.fillStyle = 'black'
+        this.game.context.font = '48px serif'
         this.game.context.fillText(this.text, 100, 190)
     }
     update() {
